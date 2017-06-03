7a5ce9dba1f7c0fccdcb660007780560e839354c
朝南 <chn13@hotmail.com>
2017-06-03 16:50:28 +0800

    Add assignment 1 for fc5 course

---
+++ fc5/module1-solution/app.js
@@ -0,0 +1,40 @@
+(function () {
+'use strict';
+
+angular.module('LunchCheck', [])
+.controller('LunchCheckController', LunchCheckController);
+LunchCheckController.$inject = ['$scope'];
+function LunchCheckController($scope) {
+  $scope.lunch = "";
+  $scope.mes = "";
+  $scope.mstyle = "";
+  $scope.itemHandle = function () {
+    var num = splitAndCount($scope.lunch);
+    if (num == 0) {
+      $scope.mes = "Please enter data first";
+      $scope.mstyle = {"color":"#D46A6A"};
+      $scope.tstyle = {"border-color":"#D46A6A"};
+    } else if (num <= 3) {
+      $scope.mes = "Enjoy!";
+      $scope.mstyle = {"color":"#55AA55"};
+      $scope.tstyle = {"border-color":"#55AA55"};
+    } else {
+      $scope.mes = "Too much!";
+      $scope.mstyle = {"color":"#55AA55"};
+      $scope.tstyle = {"border-color":"#55AA55"};
+    }
+  };
+};
+
+function splitAndCount(string) {
+  var splited = string.split(',');
+  var counter = 0;
+  for (var i = 0; i < splited.length; i++) {
+    if (splited[i]) {
+      counter++;
+    }
+  }
+  return counter;
+}
+
+})();
